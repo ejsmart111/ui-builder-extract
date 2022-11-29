c9c8af870fd6a529fdf6fe6eab11f0d991cdbc78
ejsmart111 <egbeejay@gmail.com>
2022-11-29 01:29:44 +0100

    styles adjustments

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,83 +1,84 @@
 <template>
    <div>
+      <p style="text-align: center">This is the center</p>
        <div v-show="show" id="box" class="box">
          <div class="item-box">
-            <div class="inner">
-               <div v-for="widget in widgets" :key="widget.id">
-               <p
+            <div class="inner d">
+               <div class="d" v-for="widget in widgets" :key="widget.id">
+                  <p
+                     class="items"
+                     :style="{
+                        fontSize: `${widget.size}px`,
+                        color: widget.color, left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        fontWeight: widget.fontWeight,
+                        textAlign: widget.textAlign,
+                        width: widget.width+'px',
+                        height: widget.height+'px'
+                     }"
+                     v-if="widget.type === 'Text'"
+                  >{{widget.text}}</p>
+                  <input
                   class="items"
-                  :style="{
-                     fontSize: `${widget.size}px`,
-                     color: widget.color, left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     fontWeight: widget.fontWeight,
-                     textAlign: widget.textAlign,
-                     width: widget.width+'px',
-                     height: widget.height+'px'
-                  }"
-                  v-if="widget.type === 'Text'"
-               >{{widget.text}}</p>
-               <input
-               class="items"
-                  v-else-if="widget.type === 'Input'"
-                  :placeholder="widget.placeholder"
-                  :style="{
-                     padding: widget.paddingX+'px '+widget.paddingY+'px',
-                     width: widget.width+'px',
-                     border: widget.border,
-                     left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     borderRadius: widget.borderRadius+'px'}"
-                     v-model="widget.value"
+                     v-else-if="widget.type === 'Input'"
+                     :placeholder="widget.placeholder"
+                     :style="{
+                        padding: widget.paddingX+'px '+widget.paddingY+'px',
+                        width: widget.width+'px',
+                        border: widget.border,
+                        left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        borderRadius: widget.borderRadius+'px'}"
+                        v-model="widget.value"
+                     />
+                  <div
+                     v-else-if="widget.type === 'Rounded'"
+                     class="items"
+                     :style="{
+                        backgroundColor: background(widget),
+                        padding: widget.paddingX+'px '+widget.paddingY+'px',
+                        border: widget.border,
+                        left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        borderRadius: `${widget.borderRadius}%`,
+                        height: `${widget.height}px`,
+                        width: `${widget.width}px`,
+                     }"></div>
+                  <div
+                     v-else-if="widget.type === 'Squared'"
+                     class="items"
+                     :style="{
+                        backgroundColor: background(widget),
+                        padding: widget.paddingX+'px '+widget.paddingY+'px',
+                        border: widget.border,
+                        left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        borderRadius: `${widget.borderRadius}%`,
+                        height: `${widget.height}px`,
+                        width: `${widget.width}px`,
+                     }"></div>
+                  <font-awesome-icon
+                     v-else-if="widget.type === 'Star'"
+                     class="items"
+                     :style="{
+                        color: widget.color,
+                        zIndex: '1000',
+                        opacity: widget.isTransparent?0:1,
+                        left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        fontSize: widget.size}"
+                     icon="fa-star"
                   />
-               <div
-                  v-else-if="widget.type === 'Rounded'"
-                  class="items"
-                  :style="{
-                     backgroundColor: background(widget),
-                     padding: widget.paddingX+'px '+widget.paddingY+'px',
-                     border: widget.border,
-                     left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     borderRadius: `${widget.borderRadius}%`,
-                     height: `${widget.height}px`,
-                     width: `${widget.width}px`,
-                  }"></div>
-               <div
-                  v-else-if="widget.type === 'Squared'"
-                  class="items"
-                  :style="{
-                     backgroundColor: background(widget),
-                     padding: widget.paddingX+'px '+widget.paddingY+'px',
-                     border: widget.border,
-                     left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     borderRadius: `${widget.borderRadius}%`,
-                     height: `${widget.height}px`,
-                     width: `${widget.width}px`,
-                  }"></div>
-               <font-awesome-icon
-                  v-else-if="widget.type === 'Star'"
-                  class="items"
-                  :style="{
-                     color: widget.color,
-                     zIndex: '1000',
-                     opacity: widget.isTransparent?0:1,
-                     left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     fontSize: widget.size}"
-                  icon="fa-star"
-               />
-               <button
-                  :style="{
-                     backgroundColor: background(widget),
-                     padding: widget.paddingX+'px '+widget.paddingY+'px',
-                     border: widget.border,
-                     height: `${widget.height}px`,
-                     width: `${widget.width}px`,
-                     fontSize: `${widget.size}px`,
-                     left: position(widget.x, 'x'), top: position(widget.y, 'y'),
-                     color: widget.color,
-                     borderRadius: `${widget.borderRadius}px`,
-                     fontWeight: widget.fontWeight
-                  }"
-                  class="items"
-                  v-else>{{widget.text}}</button>
+                  <button
+                     :style="{
+                        backgroundColor: background(widget),
+                        padding: widget.paddingX+'px '+widget.paddingY+'px',
+                        border: widget.border,
+                        height: `${widget.height}px`,
+                        width: `${widget.width}px`,
+                        fontSize: `${widget.size}px`,
+                        left: position(widget.x, 'x'), top: position(widget.y, 'y'),
+                        color: widget.color,
+                        borderRadius: `${widget.borderRadius}px`,
+                        fontWeight: widget.fontWeight
+                     }"
+                     class="items"
+                     v-else>{{widget.text}}</button>
                </div>
             </div>
          </div>
@@ -107,9 +108,15 @@ export default {
          return (num, type) => {
             if (type==='x'){
                if(this.width <= 500) {
-                  return num-30+'px'
+                  return num-235+'px'
                }
-               return num-200+'px'
+               if(this.width <= 300) {
+                  return num+'px'
+               }
+               // if(this.width > 1800) {
+               //    return num-250+'px'
+               // }
+               return num-180+'px'
             }
             return num-150+'px'
          }
@@ -149,9 +156,9 @@ export default {
       height: 700px;
       justify-content: center;
       align-items: center;
-      margin: 0 auto;
+      /* margin: 0 auto; */
       position: fixed;
-      width: 800px;
+      width: 100%;
    }
 
    .item-box {
@@ -170,7 +177,7 @@ export default {
    }
 
    .inner {
-      height: 500px;
+      height: 450px;
       width: 500px;
       position: relative;
    }
@@ -183,5 +190,23 @@ export default {
       100% {
          transform: scale(1);
       }
+   }
+
+   @media (max-width: 700px) {
+      .d {
+         transform: scale(0.8) !important;
+      }
+   }
+   @media (max-width: 300px) {
+      .d {
+         transform: scale(0.6) !important;
+      }
+      .inner {
+         height: 350px;
+         left: -15px;
+      }
+   }
+   .d {
+      transform: scale(1);
    }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    // css: {
-    //     extract: false,
-    // },
-    // filenameHashing: false,
-    // chainWebpack: config => {
-    //     config.plugins.delete('html');
-    //     config.plugins.delete('preload');
-    //     config.plugins.delete('prefetch');
-    // },
-    // configureWebpack: {
-    //     optimization: {
-    //         splitChunks: false,
-    //     },
-    // },
+    css: {
+        extract: false,
+    },
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.plugins.delete('html');
+        config.plugins.delete('preload');
+        config.plugins.delete('prefetch');
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false,
+        },
+    },
 })
